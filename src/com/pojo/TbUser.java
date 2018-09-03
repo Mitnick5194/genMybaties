aa9345953c31a7640ea5e30ec4cb31d80df0a59c
Mitnick5194 <qq519441630@gmail.com>
2018-09-03 20:29:37 +0800

    提交文件

---
+++ src/com/pojo/TbUser.java
@@ -0,0 +1,135 @@
+package com.pojo;
+
+import java.util.Date;
+
+public class TbUser {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String nickname;
+
+    private String synopsis;
+
+    private String sex;
+
+    private String phone;
+
+    private String email;
+
+    private Date createtime;
+
+    private Date lastactive;
+
+    private String roleids;
+
+    private String header;
+
+    private Integer mark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis == null ? null : synopsis.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getLastactive() {
+        return lastactive;
+    }
+
+    public void setLastactive(Date lastactive) {
+        this.lastactive = lastactive;
+    }
+
+    public String getRoleids() {
+        return roleids;
+    }
+
+    public void setRoleids(String roleids) {
+        this.roleids = roleids == null ? null : roleids.trim();
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header == null ? null : header.trim();
+    }
+
+    public Integer getMark() {
+        return mark;
+    }
+
+    public void setMark(Integer mark) {
+        this.mark = mark;
+    }
+}
